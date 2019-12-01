06dab0e71e2d40d44ca43a97453b630024b64918
Rubica <ewahym@gmail.com>
2019-12-01 18:38:03 +0100

    Kalendarz

---
+++ 04-Subroutines/Kalendarz.py
@@ -0,0 +1,6 @@
+#Kalendarz
+months=[(""),("January"),("February"),("March"),("April"),("May"),("June"),("July"),("Aug"),("September"),("Oct"),("Nov"),("Dec")]
+
+n = int(input("Input the month : "))
+print(months[n])
+
